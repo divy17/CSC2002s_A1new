f80580e8d6ad2d4e350d2ea45b9dcc941b4b2cf7
divy17 <69718338+divy17@users.noreply.github.com>
2020-08-17 09:10:49 +0200

    program is finding basins
    the program works accordingly still need to add the amount of basins and file output.

--- BasinThread.java
+++ BasinThread.java
@@ -1,20 +1,65 @@
-//package ParallelSum2;
+//package ForkJoinSum;
 
-public class BasinThread extends Thread {
-	  int lo; // arguments
-	  int hi;
-	  int[] arr;
+import java.util.concurrent.RecursiveTask;
 
-	  int ans = 0; // result 
+public class BasinThread extends RecursiveTask<String>  {
+	  int s1;
+     int s2; // arguments
+     int f1;
+     int f2;
+	  float[][] arr;
+	  static final int SEQUENTIAL_CUTOFF = 17;
+
+	  String ansfinal = ""; // result 
 	    
-	  BasinThread(int[] a, int l, int h) { 
-	    lo=l; hi=h; arr=a;
+	  BasinThread(float[][] a, int start1, int finish1, int start2, int finish2) { 
+	    arr = a; s1 = start1 ; f1 = finish1 ; s2 = start2 ; f2 = finish2 ; 
 	  }
 
 
-	  public void run() { //override must have this type
-	    for(int i=lo; i < hi; i++)
-	      ans += arr[i];
-	  }
-
+	  protected String compute(){// return answer - instead of run
+		  if(((f1-s1)*(f1-s1)) < SEQUENTIAL_CUTOFF) {
+         
+         String subans = "";
+         for (int i = s1; i<f1;i++) {
+          for (int j = s2; j<f2; j++) { 
+           boolean b = false;
+           
+           if ((i>s1) && (i<(f1-1)) && (j>s2) && (j<(f2-1))) {
+            float initial = 0.01f;
+            float v = arr[i][j] + initial;
+            int top   = i-1;
+            int bot   = i+1;
+            int left  = j-1;
+            int right = j+1;
+            
+            if ( (v <= arr[top][left]) && (v <= arr[top][j]) && (v <= arr[top][right]) && (v <= arr[i][left]) 
+               && (v <= arr[i][right]) && (v <= arr[bot][left]) && (v <= arr[bot][j]) && (v <= arr[bot][right]) ) {
+               b = true; }}
+               
+             if (b == true) {
+              subans = subans + Integer.toString(i) +" "+ Integer.toString(j) + "\n" ; }
+             
+		  }}
+        return subans ; }
+        
+		  else {
+			  BasinThread one   = new BasinThread(arr, 0   , f1/2, 0   , f2/2 );
+			  BasinThread two   = new BasinThread(arr, 0   , f1/2, f2/2, f2   );
+           BasinThread three = new BasinThread(arr, f1/2, f1  , 0   , f2/2 );
+           BasinThread four  = new BasinThread(arr, f1/2, f1  , f2/2, f2   );
+			  
+			  // order of next 4 lines
+			  // essential – why?
+			  two.fork();
+           three.fork();
+           four.fork();
+			  String oneAns    =  one.compute();
+			  String twoAns    =  two.join();
+           String threeAns  =  three.join();
+           String fourAns   =  four.join();
+			  return oneAns + twoAns + threeAns + fourAns  ;     
+		  }
+	 }
 }
+
--- FindBasin.java
+++ FindBasin.java
@@ -1,39 +1,72 @@
-//package ParallelSum2;
+//package ForkJoinSum;
+
+import java.util.concurrent.ForkJoinPool;
+import java.io.FileReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+import java.io.IOException;
+import java.util.NoSuchElementException;
 
 public class FindBasin {
-
-	static int sum(int[] arr, int numTs) throws InterruptedException {
-		  int ans = 0;
-		  BasinThread[] ts = new BasinThread[numTs];
-		  for(int i=0; i < numTs; i++){
-			  ts[i] = new BasinThread(arr,(i*arr.length)/numTs,
-		                             ((i+1)*arr.length)/numTs);
-			  ts[i].start();  //start, not run
-		  }
-		 
-		  for(int i=0; i < numTs; i++) { 
-			ts[i].join(); // wait for helper to finish!
-		    ans += ts[i].ans;
-		  }
-		  return ans;
-		}
+	static long startTime = 0;
+	
+	private static void tick(){
+		startTime = System.currentTimeMillis();
+	}
+	private static float tock(){
+		return (System.currentTimeMillis() - startTime) / 1000.0f; 
+	}
+   
+	static final ForkJoinPool fjPool = new ForkJoinPool();
+	static String sum(float[][] arr) throws InterruptedException {
+	  return fjPool.invoke(new BasinThread(arr, 0, arr.length, 0, arr.length ));
+	}
 
 	
 	public static void main(String[] args) {
-		int max =1000000;
-		int noThreads =4;
-		int [] arr = new int[max];
-		for (int i=0;i<max;i++) {
-			arr[i]=100;
-		}
-		try {
-		int sumArr = sum(arr,noThreads);
+    try {
+     try {
+       //FileReader fin = new FileReader(args[0]);
+       FileReader fin = new FileReader("test.txt");
+       Scanner graphFile = new Scanner( fin );
+       String line;
+       line = graphFile.nextLine( );
+       StringTokenizer st = new StringTokenizer( line );
+       int length1 = Integer.parseInt( st.nextToken( ) );
+       StringTokenizer st2 = new StringTokenizer( line );
+       int length2 = Integer.parseInt( st2.nextToken( ) );
+       
+       float [][] arr = new float[length1][length2];
+       for (int i=0;i<length1;i++) {
+        line = graphFile.nextLine( );
+        StringTokenizer st3 = new StringTokenizer( line );
+        for (int j=0;j<length2;j++) {                 
+         arr[i][j] = Float.parseFloat( st3.nextToken( ) );}}
+
+
+		tick();
+		String sumArr = sum(arr);
+		float time = tock();
+		System.out.println("Run took "+ time +" seconds");		
 		System.out.println("Sum is:");
 		System.out.println(sumArr);
-		} catch (InterruptedException e) {
+
+		tick();
+		sumArr = sum(arr);
+		time = tock();
+		System.out.println("Second run took "+ time +" seconds");		
+		System.out.println("Sum is:");
+		System.out.println(sumArr);
+      
+                 }
+             catch( IOException e )
+           { System.err.println( e ); }
+           
+           		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
 	}
 
 }
